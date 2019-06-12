256e9354cb13a57d323f2f227d70bdf2eab4d1de
Robin_tty <muellerobin95@gmail.com>
2019-06-12 23:43:10 +0200

    Add AdcGain enum

---
+++ src/BH1745Driver/AdcGain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BH1745Driver
+{
+    public enum AdcGain
+    {
+        X1 = 0b00,
+        X2 = 0b01,
+        X16 = 0b10
+    }
+}
