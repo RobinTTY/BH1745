63e48f016da417110dc559f871d50ce14946044b
Robin_tty <muellerobin95@gmail.com>
2019-06-13 20:54:41 +0200

    Add InterruptSource enum

---
+++ src/BH1745Driver/InterruptPersistence.cs
@@ -0,0 +1,22 @@
+﻿namespace BH1745Driver
+{
+    public enum InterruptPersistence : byte
+    {
+        /// <summary>
+        /// Interrupt status is toggled at each measurement end.
+        /// </summary>
+        ToggleMeasurementEnd = 0b00,
+        /// <summary>
+        /// Interrupt status is updated at each measurement end.
+        /// </summary>
+        UpdateMeasurementEnd = 0b01,
+        /// <summary>
+        /// Interrupt status is updated if 4 consecutive threshold judgments are the same.
+        /// </summary>
+        UpdateConsecutiveX4 = 0b10,
+        /// <summary>
+        /// Interrupt status is updated if 8 consecutive threshold judgments are the same.
+        /// </summary>
+        UpdateConsecutiveX8 = 0b11
+    }
+}
