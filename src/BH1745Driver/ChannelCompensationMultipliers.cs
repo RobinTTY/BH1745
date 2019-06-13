54003b077ed9c090f2da7b981935461279ece41b
Robin_tty <muellerobin95@gmail.com>
2019-06-13 20:55:26 +0200

    Add ChannelCompensationMultiplier class

---
+++ src/BH1745Driver/ChannelCompensationMultipliers.cs
@@ -0,0 +1,26 @@
+﻿namespace BH1745Driver
+{
+    public class ChannelCompensationMultipliers
+    {
+        /// <summary>
+        /// Multiplier for the red color channel.
+        /// </summary>
+        public double Red { get; set; }
+
+        /// <summary>
+        /// Multiplier for the green color channel.
+        /// </summary>
+        public double Green { get; set; }
+
+        /// <summary>
+        /// Multiplier for the blue color channel.
+        /// </summary>
+        public double Blue { get; set; }
+
+        /// <summary>
+        /// Multiplier for the clear color channel.
+        /// </summary>
+        public double Clear { get; set; }
+
+    }
+}
