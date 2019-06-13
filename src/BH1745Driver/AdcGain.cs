6c0a942e4022b36f5d66a595400e6ce330fd9f50
Robin_tty <muellerobin95@gmail.com>
2019-06-13 21:01:08 +0200

    Add comments

--- src/BH1745Driver/AdcGain.cs
+++ src/BH1745Driver/AdcGain.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BH1745Driver
+﻿namespace BH1745Driver
 {
     public enum AdcGain
     {
+        /// <summary>
+        /// Gain multiplier of 1.
+        /// </summary>
         X1 = 0b00,
+        /// <summary>
+        /// Gain multiplier of 2.
+        /// </summary>
         X2 = 0b01,
+        /// <summary>
+        /// Gain multiplier of 16.
+        /// </summary>
         X16 = 0b10
     }
 }
