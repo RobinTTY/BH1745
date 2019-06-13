3c812d3fa25fc046e99489d5deb540fae66eb0a9
Robin_tty <muellerobin95@gmail.com>
2019-06-13 20:53:51 +0200

    Add interruptSource enum

---
+++ src/BH1745Driver/InterruptSource.cs
@@ -0,0 +1,22 @@
+﻿namespace BH1745Driver
+{
+    public enum InterruptSource : byte
+    {
+        /// <summary>
+        /// The red color channel.
+        /// </summary>
+        RedChannel = 0b00,
+        /// <summary>
+        /// The green color channel.
+        /// </summary>
+        GreenChannel = 0b01,
+        /// <summary>
+        /// The blue color channel.
+        /// </summary>
+        BlueChannel = 0b10,
+        /// <summary>
+        /// The clear color channel. 
+        /// </summary>
+        ClearChannel = 0b11
+    }
+}
